9155e26152f3d4238f4b6903cc59ad8012b0678c
Maleeva_Kira <maleevakira3@gmail.com>
2026-05-29 12:47:05 +0300

    LastFix

--- IGI/LR5/cloudinary_storage.py
+++ IGI/LR5/cloudinary_storage.py
@@ -1,57 +1,59 @@
-"""
-Кастомный Cloudinary storage backend
-"""
-
 from django.core.files.storage import Storage
-from django.core.files.base import ContentFile
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-from urllib.parse import urlparse
 
 
 class CloudinaryStorage(Storage):
     
-    def __init__(self):
-        pass
-    
     def _save(self, name, content):
-        """
-        Загружает файл на Cloudinary
-        """
+        public_id = name.rsplit('.', 1)[0]
+        
         # Загружаем файл
         result = cloudinary.uploader.upload(
             content,
-            public_id=name.split('.')[0],
+            public_id=public_id,
             folder='media',
-            resource_type='auto'
+            resource_type='auto',
+            overwrite=True
         )
         
         return result['public_id']
     
     def exists(self, name):
         try:
-            cloudinary.api.resource(name)
+            public_id = f"media/{name.rsplit('.', 1)[0]}"
+            cloudinary.api.resource(public_id)
             return True
-        except cloudinary.api.NotFound:
+        except:
             return False
     
     def url(self, name):
         if not name:
             return ''
         
+        if name.startswith('media/'):
+            public_id = name
+        else:
+            public_id = f"media/{name.rsplit('.', 1)[0]}"
+        
         # Генерируем URL
-        return cloudinary.CloudinaryImage(name).build_url()
+        return cloudinary.CloudinaryImage(public_id).build_url()
     
     def delete(self, name):
         try:
-            cloudinary.uploader.destroy(name)
-        except Exception:
+            public_id = f"media/{name.rsplit('.', 1)[0]}"
+            cloudinary.uploader.destroy(public_id)
+        except:
             pass
     
     def size(self, name):
         try:
-            resource = cloudinary.api.resource(name)
+            public_id = f"media/{name.rsplit('.', 1)[0]}"
+            resource = cloudinary.api.resource(public_id)
             return resource.get('bytes', 0)
-        except Exception:
+        except:
             return 0
+    
+    def get_available_name(self, name, max_length=None):
+        return name
